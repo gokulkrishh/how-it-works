5e5434149fcdb27b17e8142ca5cc8723182f2e13
gokulkrishh <krishnangokul9@gmail.com>
2020-02-16 18:36:32 +0530

    Updated readme, pkg.json and delete old template files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# how-it-works
-How it works inside javascript feature.
+# [how-it-works](https://how-it-works.dev)
+
+> How it works inside javascript feature.
+
+### 🚀 Quick start
+
+- Develop: **npm run start**
+
+> Your site is now running at `http://localhost:8000`
+
+- Build: **npm run build**
+
+> Build is created in public folder and site is automatically deployed via netlify.
+
+##### MIT Licensed
+
+[@gokulkrishh](https://github.com/gokulkrishh)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `How it works`,
+    description: `How it works inside javascript feature.`,
+    author: `@gokulkrishh`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
