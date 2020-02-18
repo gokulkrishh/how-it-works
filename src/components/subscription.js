68fdc3d2fa275f05e2f7d1c60121814dc5714edd
gokulkrishh <krishnangokul9@gmail.com>
2020-02-18 14:05:28 +0530

    Updated the site with newsletter link

--- src/components/subscription.js
+++ src/components/subscription.js
@@ -3,10 +3,30 @@ import React from "react"
 
 const Subscription = () => (
   <div className="Subscription">
-    <h3>Subscribe here</h3>
-    <input type="text" placeholder="First name" />
-    <input type="text" placeholder="Email id" />
-    <button>Subscribe</button>
+    <h2>Subscribe</h2>
+    <p>Weekly newsletter</p>
+    <form
+      action="//dev.us4.list-manage.com/subscribe/post?u=d9011fcde206b61bac7f4af0a&amp;id=e2854f474e"
+      method="post"
+      target="_blank"
+    >
+      <input type="text" name="FNAME" id="mce-FNAME" placeholder="First name" />
+      <input
+        className="email-address"
+        name="EMAIL"
+        type="email"
+        id="mce-EMAIL"
+        placeholder="Email address"
+        required
+      />
+
+      <input
+        className="submit-btn"
+        id="mc-embedded-subscribe"
+        type="submit"
+        value="Submit"
+      />
+    </form>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,33 +19,37 @@ const IndexPage = () => (
     <p>
       When I started my career as a web developer, I struggled a lot to
       understand how a method or a feature in Javascript works internally, for
-      example <b>Promise.all</b> or <b>.bind</b> etc,. I have been asked to
-      write in white paper or implement it online in many interview's. Sometimes
+      example <b>Promise.all</b> or <b>.bind</b>, etc. I have been asked to
+      write in white paper or implement it online in many interviews. Sometimes
       I struggled and sometimes I was able to do it. I could find articles
       online here and there explaining how it works. But it was not enough.
     </p>
     <p>
-      After gaining 7 years of total experience in web development. I still
-      could not able to find a better resource on how it works internally for
-      many features in Javascript. Hence I created this site to learn more and
-      to understand even better about javascript and its internal
-      implementations.
+      In my all years of working in web development. I am still struggling to
+      find a better resource on how it works internally for many features in
+      Javascript. Hence I created this site to learn more and to understand even
+      better about javascript and its internal implementations. This will be a
+      weekly basis newsletter.
     </p>
 
     <p>
-      Hopefully you will also learn as much as me via this site. Do reply me to
-      my emails via subscription if I make a mistake or if there is better way
-      to do it. Lets keep the learning and conversation going.
+      Hopefully, you will also learn as much as me via this site. Do reply to my
+      emails via subscription if I make a mistake or if there is a betterway to
+      do it. Let's keep the learning and conversation going.
     </p>
 
     <Subscription />
 
     <p>
-      And I will make sure the email list is keep secret and I promise there
-      wont be any spamming.
+      And I will make sure the email list is kept secret and I promise there
+      won't be any spamming.
     </p>
     <h3>Cheers,</h3>
-    <h3>Gokul</h3>
+    <h3>
+      <a href="https://gokul.site" target="_blank">
+        Gokul
+      </a>
+    </h3>
   </Layout>
 )
 
