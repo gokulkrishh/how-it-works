e066858ae5f3dab9326b3493786fa542ea4b491f
gokulkrishh <krishnangokul9@gmail.com>
2020-02-18 18:25:12 +0530

    Update conents in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,31 +21,30 @@ const IndexPage = () => (
       ;).
     </p>
     <p>
-      When I started my career as a web developer, I struggled a lot to
-      understand how a method or a feature in Javascript works internally, for
-      example <b>Promise.all()</b> or <b>Object.create()</b>, etc. I have been
-      asked to write in white paper or implement it online in many interviews.
-      Sometimes I have struggled and sometimes I was able to do it.
+      When I started my career as a web developer, I struggled to understand how
+      a method or a feature in Javascript works internally, for example
+      <b>Promise.all()</b> or <b>Object.create()</b>, etc. I have been asked to
+      write these methods in white paper or implement it online in many
+      interviews. Sometimes I have struggled and sometimes I was able make it
+      work by writing in Javascript.
     </p>
     <p>
-      In my all years of working in web development. I am still struggling to
-      find a better resource on how it works internally for many features in
-      Javascript. Hence I created this site to learn more and to understand even
-      better about javascript and its internal implementations. This will be a
-      weekly basis newsletter.
+      So I created this site to learn and to understand Javascript even better
+      and to know what could be its implementations.
     </p>
 
     <p>
-      Hopefully, you will also learn as much as me via this site. Do reply to my
-      emails via subscription if I make a mistake or if there is a better way to
-      do it. Let's keep the learning and conversation going.
+      Hopefully, you will also learn as much as me via this site/newsletter. Do
+      reply to my emails via subscription or via comments in each post if there
+      is a mistake or a better way to do it. Let's keep the learning and
+      conversation going.
     </p>
 
     <Subscription />
 
     <p>
-      And I will make sure the email list is kept secret and I promise there
-      won't be any spamming!
+      And I will make sure the email list is kept secret and there won't be any
+      spamming!
     </p>
 
     <div className="layout__footer">
