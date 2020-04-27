f6500ca35b7859ed74d82403de0b5b669b084b1d
gokulkrishh <krishnangokul9@gmail.com>
2020-04-27 10:06:44 +0530

    Updated img resolution

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Subscription from "../components/subscription";
-import CoffeeImg from "./coffee.jpeg";
+import CoffeeImg from "./coffee.jpg";
 
 const IndexPage = () => {
   return (
